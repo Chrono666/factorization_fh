9a37166b94546e5b508dbb4a92c468d8cfb8a62f
Matthias Schmid-Kietreiber <matthias.schmid-kietreiber@protonmail.com>
2020-10-20 19:27:51 +0200

    little refactoring

--- src/at/MReiber/Factorization.java
+++ src/at/MReiber/Factorization.java
@@ -1,15 +1,12 @@
 package at.MReiber;
 
-import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.LinkedList;
-
 public class Factorization {
 
     public int toBitLength(long n) {
         return (int) (Math.log(n) / Math.log(2) + 1);
     }
 
+/*
     private Boolean isPrime(long n) {
         if (n <= 1) {
             return false;
@@ -21,11 +18,9 @@ public class Factorization {
         }
         return true;
     }
+*/
 
     public void factorization(long n) {
-        if (isPrime(n)) {
-            System.out.println(n + " is prime");
-        } else {
             long start = System.nanoTime();
             for (int i = 2; i < n; i++) {
                 while (n % i == 0) {
@@ -34,10 +29,9 @@ public class Factorization {
                 }
             }
             if (n > 2) {
-                long end = System.nanoTime();
                 System.out.println(n);
+                long end = System.nanoTime();
                 System.out.println("Time: " + ((end - start) / 1000000) + " ms");
             }
         }
-    }
 }
